fff7300d19d95ad1bd21e8b35f7c8c2e9bf8f4a4
fmndantas <fernandodantas1996@gmail.com>
2019-05-30 23:28:08 -0300

    send_image_data refactory using xpath

--- peoplefinder/SendData.py
+++ peoplefinder/SendData.py
@@ -10,7 +10,6 @@ from selenium.webdriver.common.keys import Keys
 
 
 def send_text_data(driver, phone, msg):
-    print(msg)
     driver.get('https://web.whatsapp.com/send?phone={}'.format(phone))
     try:
         # Verify valid number. If the number doesn't exist, raise TimeoutException
@@ -32,27 +31,25 @@ def send_image_data(driver, phone, img_path, img_label):
     # From here, assumes that the user is logged in
     if os.path.exists(img_path):
         driver.get('https://web.whatsapp.com/send?phone={}'.format(phone))
-        while True:
+        while True:  # Fast version
             try:
-                assert (len(driver.find_elements_by_class_name('rAUz7')) == 6)
+                assert (len(driver.find_elements_by_xpath('//header')) == 2)
             except AssertionError:
                 continue
             else:
                 break
         while True:
             try:
-                driver.find_elements_by_class_name('rAUz7')[4].click()
-            except ElementClickInterceptedException:
+                driver.find_elements_by_xpath('//div[@title="Attach"]')[0].click()
+            except Exception as Error:
                 continue
             else:
                 break
-        image = driver.find_element_by_class_name('_1azEi')
+        image = driver.find_elements_by_xpath('//button[@data-animate-menu-icons-item="true"]')[0]
         image.click()
         autoit.win_wait("File Upload")
         # Minimize
         autoit.win_set_state("File Upload", properties.SW_MINIMIZE)
-        # Hide
-        # autoit.win_set_state("File Upload", properties.SW_HIDE)
         sleep(2)
         autoit.control_send("File Upload", "[CLASS:Edit]", img_path, mode=0)
         sleep(2)
@@ -60,14 +57,13 @@ def send_image_data(driver, phone, img_path, img_label):
         autoit.win_close("File Upload")
         autoit.win_wait_close("File Upload")
         sleep(2)
-        img_label_box = driver.find_elements_by_class_name('_2S1VP')[0]
+        img_label_box = driver.find_elements_by_xpath('//div[@spellcheck="true"]')[0]
         if img_label:
             img_label_box.send_keys(img_label)
         img_label_box.send_keys(Keys.ENTER)
         sleep(2)
         return img_label
     else:
-        print('Imagem não existe')
         # The file is wrong
         return None
 
