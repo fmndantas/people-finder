f682a153fd2b0d9970da5f026242cec7513015b0
fmndantas <fernandodantas1996@gmail.com>
2019-06-04 18:24:48 -0300

    send_text_data and send_image_data (unfinished)

--- peoplefinder/SendData.py
+++ peoplefinder/SendData.py
@@ -2,6 +2,7 @@ import autoit
 from autoit.autoit import properties
 from time import sleep
 import os
+import pdb
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -9,12 +10,11 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, NoSuchElementException
 from selenium.webdriver.common.keys import Keys
 
-
 def send_text_data(driver, phone, msg):
     driver.get('https://web.whatsapp.com/send?phone={}'.format(phone))
     text_box_xpath = "/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div[2]/div/div[2]"
     try:
-        WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, text_box_xpath)))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, text_box_xpath)))
     except TimeoutException:
         return None
     text_box = driver.find_element(By.XPATH, text_box_xpath)
@@ -29,7 +29,7 @@ def send_image_data(driver, phone, img_path, img_label):
         clip_xpath = '/html/body/div[1]/div/div/div[4]/div/header/div[3]/div/div[2]'
         image_xpath = '/html/body/div[1]/div/div/div[4]/div/header/div[3]/div/div[2]/span/div/div/ul/li[1]/button'
         try:
-            WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, clip_xpath)))
+            WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, clip_xpath)))
             driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[4]/div/div/div[2]/h1')
         except TimeoutException:
             return None
@@ -38,14 +38,28 @@ def send_image_data(driver, phone, img_path, img_label):
         driver.find_element(By.XPATH, clip_xpath).click()
         driver.find_element(By.XPATH, image_xpath).click()
         autoit.win_wait("File Upload")
+        pdb.set_trace()
         # Minimize
         autoit.win_set_state("File Upload", properties.SW_MINIMIZE)
+        pdb.set_trace()
         autoit.control_send("File Upload", "[CLASS:Edit]", img_path, mode=0)
+        # todo pyautoit typing incorrect image address here
+        pdb.set_trace()
+        sleep(1)
         autoit.control_click("File Upload", "[CLASS:Button]")
+        pdb.set_trace()
         autoit.win_close("File Upload")
+        pdb.set_trace()
         autoit.win_wait_close("File Upload")
-        add_caption_xpath = '/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/span/div/div[2]/div/div[3]/div[1]/div[2]'
+        pdb.set_trace()
+        add_caption_xpath = '/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/span/div/div[2]/div/div[3]/div[1]'
+        try:
+            WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.XPATH, add_caption_xpath)))
+        except (TimeoutError, NoSuchElementException):
+            return None
+        pdb.set_trace()
         add_caption = driver.find_element(By.XPATH, add_caption_xpath)
+        pdb.set_trace()
         if img_label:
             add_caption.send_keys(img_label)
         add_caption.send_keys(Keys.ENTER)
