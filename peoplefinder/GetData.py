dfc287eb40b5fe5471f536713ed8463c8f18dee3
fmndantas <fernandodantas1996@gmail.com>
2019-06-04 18:25:40 -0300

    data getters functions using xpath

--- peoplefinder/GetData.py
+++ peoplefinder/GetData.py
@@ -3,59 +3,64 @@ from time import sleep
 
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException, NoSuchElementException
-
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, ElementClickInterceptedException, ElementNotInteractableException
 
 def text(driver):
-    """Get status"""
-    header = driver.find_element_by_class_name('_2y17h')
-    header.click()
-    # wait until text has loaded in the sidebar
+    """Returns contact status"""
+    header_xpath = '/html/body/div[1]/div/div/div[4]/div/header'
     while True:
         try:
-            WebDriverWait(driver, 1).until(EC.text_to_be_present_in_element(('class name', '_14oqx'), 'Mute'))
-        except TimeoutException:
+            driver.find_element(By.XPATH, header_xpath).click()
+        except ElementClickInterceptedException:
             continue
         else:
             break
-    texts = driver.find_elements_by_class_name('_14oqx')
-    if len(texts) < 4:
-        status = ''
+    sleep(1)
+    # abt_n_phn_nmbr_xpth = '/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div/div[4]/div[1]/span'
+    # try:
+    #     WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.XPATH, abt_n_phn_nmbr_xpth)))
+    # except NoSuchElementException:
+    #     return None
+    status_xpath = '/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div/div[4]/div[2]/div/div'
+    number_xpath = '/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div/div[4]/div[3]/div/div'
+    try:
+        driver.find_element(By.XPATH, number_xpath)
+    except NoSuchElementException:
+        return None  # No status
     else:
-        status = texts[2].text
-    return status
+        return driver.find_element(By.XPATH, status_xpath).text  # There's status
 
 
 def screenshot(driver, savedir, phone):
-    # wait a little bit to load images
-    sleep(3.0)
-    picbox = driver.find_element_by_class_name('_2u2Mg')
-    piclist = picbox.find_elements_by_tag_name('img')
-    if len(piclist) == 0:
-        filename = ''
+    """Performs a screenshot in number profile if it has photo and returns the name of the resulting file"""
+    filename = os.path.join(savedir, phone + '.png')
+    picbox_xpath = '/html/body/div[1]/div/div/div[2]/div[3]/span/div/span/div/div/div/div[1]/div[1]/div/img'
+    pic_xpath = '/html/body/div[1]/div/span[3]/div/div/div[2]/div/div/div/div/img'
+    try:
+        driver.find_element(By.XPATH, picbox_xpath).click()
+    except (NoSuchElementException, ElementNotInteractableException):  # No profile pic
+        return None
     else:
-        piclist[0].click()
-        pic = driver.find_element_by_class_name('_1zH0g')
-        filename = savedir + phone + '.png'
-        pic.screenshot(filename)
-    return filename
+        sleep(2)
+        driver.find_element(By.XPATH, pic_xpath).screenshot(filename)  # There's profile pic
+        return filename
 
 
-def get_data(driver, phone, savedir, stream):
+def get_data(driver, phone, savedir):
     """Takes the photo and number of a given contact in WhatsApp"""
-    if not stream:
-        driver.get('https://web.whatsapp.com/send?phone={}'.format(phone))
-        sleep(2.0)
-        try:
-            WebDriverWait(driver, 30).until_not(EC.presence_of_element_located(('class name', '_2dA13')))
-        except TimeoutException:
-            return None
+    driver.get('https://web.whatsapp.com/send?phone={}'.format(phone))
+    header_xpath = '/html/body/div[1]/div/div/div[4]/div/header'
+    try:
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, header_xpath)))
+    except TimeoutException:  # Invalid number
+        return None
     try:
         status = text(driver)
-    except NoSuchElementException:
+        filename = screenshot(driver, savedir, phone)
+        if not filename:
+            return None
+        else:
+            return filename, status
+    except ElementClickInterceptedException:
         return None
-
-    photo_dir = os.path.join(savedir, 'photos' + os.sep)
-    filename = screenshot(driver, photo_dir, phone)
-
-    return filename, status
