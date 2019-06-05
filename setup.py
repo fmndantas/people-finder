daa28ee72e4cccb50d5d81764e512f67b5732586
fmndantas <fernandodantas1996@gmail.com>
2019-06-05 16:54:48 -0300

    adding peoplefinder and peoplechatter folders from setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 import setuptools
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from peoplefinder.common import make_dirs
+
+class CustomInstall(install):
+	def run(self):
+		install.run(self)		
+		print('Running custom install...')		
+		make_dirs()
+		
+class CustomDevelop(develop):
+	def run(self):		
+		develop.run(self)		
+		print('Running custom install in develop mode...')
+		make_dirs()
 
 with open("README.md", 'r') as f:
     long_description = f.read()
@@ -15,7 +30,11 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     scripts=["bin/peoplefinder", "bin/peopleuploader", "bin/peoplechatter"],
     install_requires=['numpy>=1.11', 'selenium', 'tqdm', 'python-telegram-bot', 'astropy', 'pyautoit', 'pytest', 'xlrd'],
-    classifiers=(
+	cmdclass={
+		'install': CustomInstall,
+		'develop': CustomDevelop,
+	},    
+	classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: Unix, Windows",
